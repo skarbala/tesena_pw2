67c64e34a12da26526c56d0c9fdef1a777690e66
martinskarbala <martin@supernova.io>
2026-06-09 13:13:59 +0200

    test: enhance GET /spells test by adding validation for spell properties and improving query parameter handling

--- tests/api/spells.spec.ts
+++ tests/api/spells.spec.ts
@@ -1,25 +1,42 @@
 // https://documenter.getpostman.com/view/6199862/SzYewFs9
 
 // zakladny smoke test -> 200 OK response && obsauje data
-// overit query parameter type
-// overit query parameter isUnforigvable
-// overit limit
+
 // overit kombinaciu paramterov
-import test, { expect } from "@playwright/test";
+import test, { expect, request } from "@playwright/test";
 
 test.describe("GET /spells", () => {
-  test("return 200 and contain list of spells", async ({ request }) => {
+  test("returns 200 and contain list of spells", async ({ request }) => {
     const response = await request.get("http://localhost:3000/spells");
     await expect(response).toBeOK();
-    //3. overim ze data nie su prazdne
 
     const responseJson = await response.json();
-    console.log(responseJson.length);
-
-    //overim ze nie je napr undefined
     expect(responseJson).toBeTruthy();
     //overim ze tam je aspon 1 kuzlo
     expect(responseJson.length).toBeGreaterThan(0);
     expect(responseJson.length).toEqual(151);
+
+    //overim ze kazde kuzlo ma vyplneny effect a nazov
+    responseJson.forEach((spellItem) => {
+      expect(spellItem.effect).toBeTruthy();
+      expect(spellItem.id).toBeTruthy();
+      expect(spellItem.spell).toBeTruthy();
+      expect(spellItem.type).toBeTruthy();
+      expect(spellItem.isUnforgivable).toBeDefined();
+    });
+  });
+
+  test("query param", async ({ request }) => {
+    const response = await request.get("http://localhost:3000/spells", {
+      params: {
+        type: "Jinx",
+      },
+    });
+
+    const responseJson = await response.json();
+    console.log(responseJson);
   });
 });
+// S: overit query parameter type
+// S: overit query parameter isUnforigvable
+// S: overit limit
