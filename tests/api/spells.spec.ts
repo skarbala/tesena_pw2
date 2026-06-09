6bb44c62a5cb456c01f14219692d190836854b68
martinskarbala <martin@supernova.io>
2026-06-09 12:47:34 +0200

    test: improve GET /spells test by ensuring response handling and validation

--- tests/api/spells.spec.ts
+++ tests/api/spells.spec.ts
@@ -9,9 +9,17 @@ import test, { expect } from "@playwright/test";
 
 test.describe("GET /spells", () => {
   test("return 200 and contain list of spells", async ({ request }) => {
-    request.get("http://localhost:3000/spells");
-    //1.zavolam get request na http://localhost:3000/spells
-    //2. overim ze odpoved je ok
+    const response = await request.get("http://localhost:3000/spells");
+    await expect(response).toBeOK();
     //3. overim ze data nie su prazdne
+
+    const responseJson = await response.json();
+    console.log(responseJson.length);
+
+    //overim ze nie je napr undefined
+    expect(responseJson).toBeTruthy();
+    //overim ze tam je aspon 1 kuzlo
+    expect(responseJson.length).toBeGreaterThan(0);
+    expect(responseJson.length).toEqual(151);
   });
 });
