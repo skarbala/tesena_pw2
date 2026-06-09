104eae2fb232a00c07043db6076a62fc8702bcc4
martinskarbala <martin@supernova.io>
2026-06-09 13:28:07 +0200

    test: enhance GET /spells tests by improving parameter handling and adding validation for isUnforgivable and limit

--- tests/api/spells.spec.ts
+++ tests/api/spells.spec.ts
@@ -3,7 +3,7 @@
 // zakladny smoke test -> 200 OK response && obsauje data
 
 // overit kombinaciu paramterov
-import test, { expect, request } from "@playwright/test";
+import test, { expect } from "@playwright/test";
 
 test.describe("GET /spells", () => {
   test("returns 200 and contain list of spells", async ({ request }) => {
@@ -26,7 +26,7 @@ test.describe("GET /spells", () => {
     });
   });
 
-  test("query param", async ({ request }) => {
+  test("get spells by type param", async ({ request }) => {
     const response = await request.get("http://localhost:3000/spells", {
       params: {
         type: "Jinx",
@@ -34,9 +34,36 @@ test.describe("GET /spells", () => {
     });
 
     const responseJson = await response.json();
-    console.log(responseJson);
+    expect(responseJson.length).toBeGreaterThan(0);
+
+    responseJson.forEach((spellItem) => {
+      expect(spellItem.type).toEqual("Jinx");
+    });
+  });
+
+  test("get spells by isUnforgivable param", async ({ request }) => {
+    const response = await request.get("http://localhost:3000/spells", {
+      params: {
+        isUnforgivable: true,
+      },
+    });
+
+    const responseJson = await response.json();
+    expect(responseJson.length).toEqual(3);
+
+    responseJson.forEach((spellItem) => {
+      expect(spellItem.isUnforgivable).toEqual(true);
+    });
+  });
+
+  test("get spells by limit param", async ({ request }) => {
+    const response = await request.get("http://localhost:3000/spells", {
+      params: {
+        limit: 50,
+      },
+    });
+
+    const responseJson = await response.json();
+    expect(responseJson.length).toEqual(50);
   });
 });
-// S: overit query parameter type
-// S: overit query parameter isUnforigvable
-// S: overit limit
