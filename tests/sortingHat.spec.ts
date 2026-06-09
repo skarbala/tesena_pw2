085f4211b81cca8ca4a8be706245c14458e9b61d
martinskarbala <martin@supernova.io>
2026-06-09 11:00:23 +0200

    test: refine student sorting test by removing console logs and improving response validation

--- tests/sortingHat.spec.ts
+++ tests/sortingHat.spec.ts
@@ -3,23 +3,17 @@ import test, { expect } from "@playwright/test";
 test("it sorts student", async ({ page }) => {
   await page.goto("http://localhost:8080/#/sortingHat");
   await page.locator('[data-test="sort-button"]').click();
-
   const response = await page.waitForResponse(
     "http://localhost:3000/sortingHat",
   );
   expect(response.status()).toBe(200);
-  //1. vytiahnem telo odpovede
   const responseBody = await response.json();
-  console.log(responseBody);
-  console.log(responseBody.house);
-
-  //samostatne
-  //2. overim ze hlaska a fakulta su spravne zobrazene na zaklade dat z odpovede
-
   await expect(page.locator('[data-test="result-message"]')).toBeVisible();
   await expect(page.locator('[data-test="result-message"]')).toHaveText(
-    "data z odpovede",
+    responseBody.sortingHatSays,
   );
-
   await expect(page.locator('[data-test="house-result"]')).toBeVisible();
+  await expect(page.locator('[data-test="house-result"]')).toHaveText(
+    responseBody.house,
+  );
 });
